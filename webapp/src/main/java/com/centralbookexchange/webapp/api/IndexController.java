a76ff262be6bf0b05f6e1844626891fbc2d19b1d
Rachel Luker <rluker@exactsciences.com>
2019-10-30 13:23:18 -0600

    Working on connecting index.html to the IndexController.java in order to grab data using Thymeleaf.

---
+++ webapp/src/main/java/com/centralbookexchange/webapp/api/IndexController.java
@@ -0,0 +1,24 @@
+package com.centralbookexchange.webapp.api;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("index")
+    public ModelAndView getWatchlist() {
+
+        String viewName = "index";
+
+        Map<String, Object> model = new HashMap<String, Object>();
+
+        model.put("numberOfBooks", "1234");
+
+        return new ModelAndView(viewName , model);
+    }
+}
