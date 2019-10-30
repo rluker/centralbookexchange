792a843774807d773eb3f8da3ea97e2071c708d8
Rachel Luker <rluker@exactsciences.com>
2019-10-30 13:56:51 -0600

    Updated the static and template folders. Also correctly connected index.html to Thymeleaf.

--- webapp/src/main/java/com/centralbookexchange/webapp/api/IndexController.java
+++ webapp/src/main/java/com/centralbookexchange/webapp/api/IndexController.java
@@ -5,10 +5,16 @@ import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
 
     @GetMapping("index")
     public ModelAndView getWatchlist() {
