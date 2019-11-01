e4a6aaa917b33aa9a3c01803b48b3c0019bb4479
Rachel Luker <rluker@exactsciences.com>
2019-11-01 09:35:18 -0600

    Changed the way the app searching for /resources. Updated some html/css not that that was functioning properly.

--- webapp/src/main/java/com/centralbookexchange/webapp/api/IndexController.java
+++ webapp/src/main/java/com/centralbookexchange/webapp/api/IndexController.java
@@ -6,12 +6,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class IndexController 
+public class IndexController
 {
     @RequestMapping("/cbe")
-    public ModelAndView getNumBooks() 
-    {
-        ModelAndView mav = new ModelAndView("index");
+    public String index(){
+        return "index";
+    }
+
+    //    @RequestMapping("/cbe")
+    @GetMapping("/cbe")
+    public ModelAndView getNumBooks() {
+        ModelAndView mav = new ModelAndView(index());
         mav.addObject("numOfBooks", "1234");
         return mav;
     }
