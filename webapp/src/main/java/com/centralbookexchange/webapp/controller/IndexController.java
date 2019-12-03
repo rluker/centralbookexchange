9a1db0a1f214cf951da5b32d9c9734fe7b293772
Ratheria <arianafairbanks@gmail.com>
2019-12-02 18:39:36 -0700

    Removed old tutorials and files.

--- webapp/src/main/java/com/centralbookexchange/webapp/controller/IndexController.java
+++ webapp/src/main/java/com/centralbookexchange/webapp/controller/IndexController.java
@@ -1,18 +1,14 @@
-package com.centralbookexchange.webapp.api;
+package com.centralbookexchange.webapp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.centralbookexchange.webapp.db.EmailRepository;
 import com.centralbookexchange.webapp.model.Email;
-import com.centralbookexchange.webapp.model.Greeting;
 
 
 @Controller
--- webapp/src/main/java/com/centralbookexchange/webapp/db/PersonRepo.java
+++
@@ -1,8 +0,0 @@
-package com.centralbookexchange.webapp.db;
-
-import com.centralbookexchange.webapp.db.PersonDB;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PersonRepo extends CrudRepository<PersonDB, Integer> {
-    PersonDB findByUserName(String name);
-}
