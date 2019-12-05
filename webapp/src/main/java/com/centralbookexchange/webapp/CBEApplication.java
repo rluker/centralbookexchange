ff405e81a6745c85b59e877d6dd32de400d3f672
Ratheria <arianafairbanks@gmail.com>
2019-12-05 01:57:25 -0700

    Now packages as .war for jsp compatability

--- webapp/src/main/java/com/centralbookexchange/webapp/CBEApplication.java
+++ webapp/src/main/java/com/centralbookexchange/webapp/CBEApplication.java
@@ -1,19 +1,8 @@
 package com.centralbookexchange.webapp;
 
-import com.centralbookexchange.webapp.db.*;
-import org.springframework.beans.factory.annotation.*;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
-
-import javax.sql.DataSource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.config.*;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 
 @SpringBootApplication
