8a10ac5f0a30e38a877a21cb52b1627ef3126277
Rachel Luker <rluker@exactsciences.com>
2019-12-05 13:31:36 -0700

    Adding error.html mapping for /error pages.

--- webapp/src/main/java/com/centralbookexchange/webapp/CBEApplication.java
+++ webapp/src/main/java/com/centralbookexchange/webapp/CBEApplication.java
@@ -1,15 +1,13 @@
 package com.centralbookexchange.webapp;
 
 import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import java.sql.SQLException;
+import org.springframework.boot.autoconfigure.*;
 
 
 @SpringBootApplication
 public class CBEApplication 
 {
-    public static void main( String[] args ) throws SQLException 
-    {
+    public static void main( String[] args ) {
         SpringApplication.run( CBEApplication.class, args );
     }
 
